f5675b66b55b481c650c32229edf08f7d66b9bda
Cleo Naughton <coopernaughton@gmail.com>
2022-06-27 17:35:27 -0600

    he can get the coin

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const can = document.getElementById("game");
 const ctx = can.getContext("2d");
+
 ctx.fillStyle = "red";
-ctx.fillRect(100, 100, 50, 50);
+ctx.fillRect(275, 400, 50, 50);
+
+ctx.fillStyle = "gold";
+
+const circle = new Path2D();
+circle.ellipse(300, 300, 20, 25, 0, 0, 2*Math.PI)
+ctx.fill(circle)
