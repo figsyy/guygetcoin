683ffad6e5e72560a306c76d8b7fe8cb1ce05f43
Cleo Naughton <coopernaughton@gmail.com>
2022-06-28 16:36:03 -0600

    progress!
    Co-authored-by: Patrick Naughton <patrickjnaughton@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const can = document.getElementById("game");
 const ctx = can.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(275, 400, 50, 50);
+function clear() {
+  ctx.fillStyle = "#33a";
+  ctx.fillRect(0, 0, 600, 600);
+}
 
-ctx.fillStyle = "gold";
+const coin = {
+  name: "coin",
+  x: 300,
+  y: 300,
+  rw: 20,
+  rh: 25,
+  fillStyle: "gold"
+};
 
-const circle = new Path2D();
-circle.ellipse(300, 300, 20, 25, 0, 0, 2*Math.PI)
-ctx.fill(circle)
+function drawcoin() {
+  const { x, y, rw, rh, fillStyle } = coin;
+  ctx.fillStyle = fillStyle;
+  const path = new Path2D();
+  path.ellipse(x, y, rw, rh, 0, 0, 2 * Math.PI);
+  ctx.fill(path);
+}
+
+const guy = {
+  name: "guy",
+  x: 275,
+  y: 400,
+  w: 50,
+  h: 50,
+  fillStyle: "red"
+};
+
+function drawguy() {
+  const { x, y, w, h, fillStyle } = guy;
+  ctx.fillStyle = fillStyle;
+  const path = new Path2D();
+  path.rect(x, y, w, h);
+  ctx.fill(path);
+}
+
+function moveguy(dx, dy) {
+  guy.x += dx;
+  guy.y += dy;
+}
+
+function draw() {
+  clear();
+  drawcoin();
+  drawguy();
+}
+moveguy(35, 35);
+
+draw();
